9c571f3b9ff0bc3b067f0ac040efd4a6a1973ef9
Vladimir-Ciuculescu <vladimir.ciuculescu@gmail.com>
2023-06-19 18:08:23 +0300

    Added Supabase

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { Text } from "react-native-ui-lib";
+import { supabase } from "./database/supabase";
+import { useEffect } from "react";
 
 export default function App() {
+  const getVladi = async () => {
+    const { data } = await supabase.from("users").select(`*`);
+
+    console.log(data);
+  };
+  useEffect(() => {
+    getVladi();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
---
+++ database/supabase.ts
@@ -0,0 +1,25 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "@env";
+
+const ExposeSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: ExposeSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
